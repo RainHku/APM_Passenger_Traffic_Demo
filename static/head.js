d0309b0eb652302bda525c24186929751a20d838
xiayu <xiayu666@connect.hku.hk>
2023-04-24 17:00:12 +0800

    update

---
+++ static/head.js
@@ -0,0 +1,10 @@
+document.body.innerHTML = '\
+<div id="title">\
+    <h1>资源中心</h1>\
+</div>\
+<hr class="hr_line">\
+<ul id="nav">\
+    <li><a href="video.html">视频</a></li>\
+</ul>\
+<hr class="hr_line">';
+// console.log("脚本执行成功");
